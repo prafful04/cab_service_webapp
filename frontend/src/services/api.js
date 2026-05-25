eb13947967e9b1031b9166a93c6dd1a054ba2fd1
Prafful Jadhav <prafful.jadhav1980@gmail.com>
2026-05-25 16:06:26 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: '/api' })
+const API = axios.create({
+  baseURL: 'https://cab-service-webapp.onrender.com/api'
+})
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
