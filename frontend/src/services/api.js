512d22ec7b445936c11376fe97c78e5e7cdc31af
Prafful Jadhav <prafful.jadhav1980@gmail.com>
2026-05-25 16:55:08 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://cab-service-webapp.onrender.com/api'
+  baseURL: 'https://cab-service-weba.onrender.com/api'
 })
 
 API.interceptors.request.use((config) => {
