6a36bdc77b73815ddc98392655b4ac6fd2b28b63
Prafful Jadhav <prafful.jadhav1980@gmail.com>
2026-08-01 02:24:05 +0530

    Add missing import for Bean annotation

--- backend/src/main/java/com/namasvi/cab/config/CloudinaryConfig.java
+++ backend/src/main/java/com/namasvi/cab/config/CloudinaryConfig.java
@@ -1,7 +1,7 @@
 package com.namasvi.cab.config;
 
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Bean;
 @Configuration
 public class CloudinaryConfig {
 
