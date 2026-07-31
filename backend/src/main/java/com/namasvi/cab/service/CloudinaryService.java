5b6423254678577a8423f81ca0ab2e556726191e
Prafful Jadhav <prafful.jadhav1980@gmail.com>
2026-08-01 02:33:24 +0530

    Refactor upload method to use Cloudinary API

--- backend/src/main/java/com/namasvi/cab/service/CloudinaryService.java
+++ backend/src/main/java/com/namasvi/cab/service/CloudinaryService.java
@@ -1,5 +1,15 @@
 package com.namasvi.cab.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
 @Service
 public class CloudinaryService {
 
@@ -10,7 +20,8 @@ public class CloudinaryService {
 
         Map<?, ?> result = cloudinary.uploader().upload(
                 file.getBytes(),
-                ObjectUtils.emptyMap());
+                ObjectUtils.emptyMap()
+        );
 
         return result.get("secure_url").toString();
     }
