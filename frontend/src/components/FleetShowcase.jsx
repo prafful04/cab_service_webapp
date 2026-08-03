8a305df10c3ac57237bfc4799489bcd8d31f0ce7
Prafful Jadhav <prafful.jadhav1980@gmail.com>
2026-08-03 16:52:45 +0530

    Update FleetShowcase.jsx

--- frontend/src/components/FleetShowcase.jsx
+++ frontend/src/components/FleetShowcase.jsx
@@ -36,9 +36,8 @@ export default function FleetShowcase() {
               transition={{ delay: index * 0.1 }}
               className="card overflow-hidden group"
             >
-              <d<div> <img src={vehicle.img} alt= {vehicle.name} className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"/></div>
-                {/* <div className="absolute inset-0 bg-primary-500/10 group-hover:bg-primary-500/20 transition-all" />
-                <div className="text-6xl text-primary-300 z-10">🚗</div> */}
+              <div> <img src={vehicle.img} alt= {vehicle.name} className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500"/></div>
+             
                 <div className="absolute top-3 right-3 bg-yellow-500 text-primary-500 text-xs font-bold px-3 py-1 rounded-full z-10">
                   {vehicle.type}
                 </div>
